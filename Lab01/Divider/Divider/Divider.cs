4dace1293110f4125766a48a1978bf9fe0cbc1ea
Vladonchik <116621986+Churilin@users.noreply.github.com>
2024-05-21 20:39:49 +0500

    Add files via upload

---
+++ Lab01/Divider/Divider/Divider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Divider
+{
+    internal class DivedIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Пожалуйста, введите первое число: ");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+
+                Console.WriteLine("Пожалуйста, введите второе число:");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+
+                int k = i / j;
+                Console.WriteLine($"Результат деления {0} на {1} равно { 2 }", i, j, k);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключительная ситуация: {0}",e.Message);
+            }
+        }
+    }
+}
