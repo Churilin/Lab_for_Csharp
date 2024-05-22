7c814a39c0fcfe17669d8d2cad9f6eaebb402043
Vladonchik <116621986+Churilin@users.noreply.github.com>
2024-05-22 19:12:55 +0500

    Add files via upload

---
+++ Lab02/Distance/Distance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Distance
+{
+    class Distance
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите футы: ");
+            int feet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите дюймы: ");
+            int inches = Convert.ToInt32(Console.ReadLine());
+            int totalInches = feet * 12 + inches;
+            int totalFeet = totalInches / 12;
+            int otherInches = totalInches % 12;
+            Console.WriteLine($"Сумма: {totalFeet}'-{otherInches}\"");
+        }
+    }
+}
