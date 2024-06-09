5e4a03005ac106c241e8e6eaad06aeaad29ee65d
Vladonchik <116621986+Churilin@users.noreply.github.com>
2024-06-10 00:10:11 +0500

    Add files via upload

---
+++ Lab08/CONSTRUCTORS/BankTransaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class BankTransaction
+{
+    private readonly decimal amount;
+    private readonly DateTime when;
+    public BankTransaction(decimal tranAmount)
+    {
+        amount = tranAmount;
+        when = DateTime.Now;
+    }
+    public decimal Amount()
+    {
+        return amount;
+    }
+
+    public DateTime When()
+    {
+        return when;
+    }
+}
